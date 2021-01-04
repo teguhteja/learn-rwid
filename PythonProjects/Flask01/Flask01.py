6e94504520745a71813d6b81a714cc0b8fb6d981
IB Teguh TM <teguh.teja@gmail.com>
2021-01-04 16:19:20 +0800

    Add sign up

--- PythonProjects/Flask01/Flask01.py
+++ PythonProjects/Flask01/Flask01.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import model
 
 app = Flask(__name__)
 
@@ -6,20 +7,36 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'GET':
-        return render_template('index.html')
+        return render_template('index.html', message='Aloha, Aloha!!!')
     else:
         username = request.form['username']
         password = request.form['password']
-        if username == 'A' and password == '1':
-            return render_template('index.html', message='Login successful')
+        db_password = model.check_pw(username)
+
+        if password == db_password:
+            message = model.show_color(username)
+            return render_template('football.html', message=message)
         else:
-            error_message = 'Hint: He curses a lot'
+            error_message = 'Hint: He curses a lot.'
             return render_template('index.html', message=error_message)
 
 
 @app.route('/about', methods=['GET'])
 def about():
     return render_template('about.html')
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'GET':
+        message = 'Please sign up!'
+        return render_template('signup.html', message=message)
+    else:
+        username = request.form["username"]
+        password = request.form["password"]
+        favorite_color = request.form["favorite_color"]
+        message = model.signup(username, password, favorite_color)
+        return render_template('signup.html', message=message)
 
 
 app.run(port=7000, debug=True)
